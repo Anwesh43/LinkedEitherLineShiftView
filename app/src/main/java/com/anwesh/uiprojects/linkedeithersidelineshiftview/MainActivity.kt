6054dedc86f3002d7e387145ad7d0b96429c9629
Anwesh43 <anweshthecool0@gmail.com>
2019-07-17 10:47:36 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedeithersidelineshiftview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedeithersidelineshiftview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedeithersidelineshiftview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.eitherlineshiftview.EitherLineShiftView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         EitherLineShiftView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
