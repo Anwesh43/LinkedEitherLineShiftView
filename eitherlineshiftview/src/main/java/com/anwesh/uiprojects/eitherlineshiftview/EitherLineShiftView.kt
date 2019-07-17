6432be9d93c99d62eaa3c4fe1606aab159b9d95a
Anwesh43 <anweshthecool0@gmail.com>
2019-07-17 09:37:17 +0530

    adding constants and libraries

--- eitherlineshiftview/src/main/java/com/anwesh/uiprojects/eitherlineshiftview/EitherLineShiftView.kt
+++ eitherlineshiftview/src/main/java/com/anwesh/uiprojects/eitherlineshiftview/EitherLineShiftView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.eitherlineshiftview
 /**
  * Created by anweshmishra on 17/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 3
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
