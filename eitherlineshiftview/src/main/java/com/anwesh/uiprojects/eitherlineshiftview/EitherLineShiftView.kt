528f3212cb7295b8ff65107312d3e4314f49f725
Anwesh43 <anweshthecool0@gmail.com>
2019-07-17 10:38:54 +0530

    adding EitherLineShift class

--- eitherlineshiftview/src/main/java/com/anwesh/uiprojects/eitherlineshiftview/EitherLineShiftView.kt
+++ eitherlineshiftview/src/main/java/com/anwesh/uiprojects/eitherlineshiftview/EitherLineShiftView.kt
@@ -169,4 +169,28 @@ class EitherLineShiftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class EitherLineShift(var i : Int) {
+
+        private val root : ELSNode = ELSNode(0)
+        private var curr : ELSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
