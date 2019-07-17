606101de7fc8a38daa79bbc67bf5136ea5d6ad20
Anwesh43 <anweshthecool0@gmail.com>
2019-07-17 10:18:00 +0530

    adding EitherLineShiftView class

--- eitherlineshiftview/src/main/java/com/anwesh/uiprojects/eitherlineshiftview/EitherLineShiftView.kt
+++ eitherlineshiftview/src/main/java/com/anwesh/uiprojects/eitherlineshiftview/EitherLineShiftView.kt
@@ -32,3 +32,21 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float {
     return mirrorValue(a, b) * dir * scGap
 }
+
+class EitherLineShiftView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
